f2c423711f5dcc86a6bb141cda636e79f85ddfce
sadbiy871 <kc3515520@gmail.com>
2025-06-08 19:54:29 +0500

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`
-Don't Forgot To Give Star SK-HACKER
+Don't Forgot To Give Star SK-HACKER-BOT
 
  Server running on http://localhost:` + PORT)
 })
